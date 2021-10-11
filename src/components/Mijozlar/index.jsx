3311cc20c8b5e9629564be176bef3ff9e3069d53
AbduvoiotovAsadbek <abduvoitovasadbek3@gmail.com>
2021-10-11 11:51:50 +0500

    Mijzolar Navbar Done

---
+++ src/components/Mijozlar/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "./style";
+import Navbar from "./Navbar";
+export const Mijozlar = () => {
+  return (
+    <Container>
+      <Navbar />
+    </Container>
+  );
+};
+export default Mijozlar;
