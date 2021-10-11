de673c90b999c6ee6b39798debfc5ecf5f1b7da8
AbduvoiotovAsadbek <abduvoitovasadbek3@gmail.com>
2021-10-11 14:35:54 +0500

    Generic Updated

---
+++ src/components/Generic/Edit/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container, IconEdit } from "./style";
+const Edit = () => {
+  return (
+    <Container>
+      <IconEdit></IconEdit>
+    </Container>
+  );
+};
+export default Edit;
