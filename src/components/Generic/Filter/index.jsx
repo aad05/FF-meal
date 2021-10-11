5c502b6da6eaac49e1862ddab802adb2cbad1814
AbduvoiotovAsadbek <abduvoitovasadbek3@gmail.com>
2021-10-11 09:47:10 +0500

    Generic Filter added

---
+++ src/components/Generic/Filter/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container, IconFilter } from "./style";
+const Filter = () => {
+  return (
+    <Container>
+      <IconFilter></IconFilter>
+    </Container>
+  );
+};
+export default Filter;
