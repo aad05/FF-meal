db1ff618ee78b0b800235c0c0c21e4ec7cdc116a
AbduvoiotovAsadbek <abduvoitovasadbek3@gmail.com>
2021-10-12 14:36:42 +0500

    Done

---
+++ src/components/Kategoriya/Body/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import KANavbar from "../KANavbar";
+import DND from "../DND";
+export const Body = () => {
+  return (
+    <div>
+      <KANavbar />
+      <DND />
+    </div>
+  );
+};
+export default Body;
