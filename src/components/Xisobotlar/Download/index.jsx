55daff7cd0273a6d111fcca7349553efc485d122
Ahrorbek <akhrorcodrr@gmail.com>
2021-10-13 11:52:55 +0500

    XisobotNavbar  updated

--- src/components/Xisobotlar/Download/index.jsx
+++ src/components/Xisobotlar/Download/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Download = () => {
     return (
         <div>
-            Download
+           Asadbek bu --- Download
         </div>
     )
 }
--- src/components/Xisobotlar/Stats/index.jsx
+++ src/components/Xisobotlar/Stats/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
  const Stats = () => {
     return (
         <div>
-            Stats
+           Asadbek bu --- Stats
         </div>
     )
 }
