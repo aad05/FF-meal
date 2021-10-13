9bd7ad2e182f801b22a922fdf41dfe30cf4aceca
AbduvoiotovAsadbek <abduvoitovasadbek3@gmail.com>
2021-10-13 20:02:54 +0500

    Done By Asadbek

---
+++ src/components/Katalog/Body/ImageConfig.js
@@ -0,0 +1,11 @@
+import fileDefault from "../../../assets/img/file-blank-solid-240.png";
+import fileCSS from "../../../assets/img/file-css-solid-240.png";
+import filePdf from "../../../assets/img/file-pdf-solid-240.png";
+import filePng from "../../../assets/img/file-png-solid-240.png";
+
+export const ImageConfig = {
+  default: fileDefault,
+  pdf: filePdf,
+  png: filePng,
+  css: fileCSS,
+};
