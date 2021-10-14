44743267cd0c57a77f168a8c767377319dff610e
AbduvoiotovAsadbek <abduvoitovasadbek3@gmail.com>
2021-10-14 17:44:09 +0500

    Loader Uploaded

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import GridLoader from "react-spinners/GridLoader";
+import Root from "./root";
+export const App = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 8000);
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <div className="loader">
+          <GridLoader color={"#FCB600"} loading={loading} size={50} />
+        </div>
+      ) : (
+        <Root />
+      )}
+    </div>
+  );
+};
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import MainContext from "./context";
 import "./index.css";
-import Root from "./root";
-
+import App from "./App";
 ReactDOM.render(
   <React.StrictMode>
     <MainContext>
-      <Root />
+      <App />
     </MainContext>
   </React.StrictMode>,
   document.getElementById("root")
